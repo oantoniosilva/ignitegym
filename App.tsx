54986d44d1ced83f8a8d0ef88e50416d09f15b9a
Antonio <antoniopsilva083@gmail.com>
2022-11-14 01:08:18 -0300

    feat: create theme for application

--- App.tsx
+++ App.tsx
@@ -2,22 +2,23 @@ import { View, StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
-  const [fontLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold
   })
 
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar 
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       
-      <Loading />
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
