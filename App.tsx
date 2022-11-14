70d11fb6229a6b2857d3610fc4308d782dec05c7
Antonio <antoniopsilva083@gmail.com>
2022-11-14 00:27:58 -0300

    chore: add expo-google-fonts and settings

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold
+  })
+
   return (
-    <View>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {fontLoaded ? <Text>Working on your app!</Text> : <View />}
     </View>
   );
 }
