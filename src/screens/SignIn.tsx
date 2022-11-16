e03924a6cd5359999f9eba2edd699fa088aa9a55
Antonio <antoniopsilva083@gmail.com>
2022-11-16 02:19:35 -0300

    feat: create input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,9 @@
-import { VStack, Image } from 'native-base';
+import { VStack, Image, Text, Center, Heading } from 'native-base';
 
-import BackgroundImg from '@assets/background.png'
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
+
+import { Input } from '@components/Input';
 
 export function SignIn() {
   return (
@@ -11,6 +14,18 @@ export function SignIn() {
         resizeMode='contain'
         position='absolute'
       />
+
+      <Center my={24}>
+        <LogoSvg />
+        <Text color="gray.100" fontSize="sm">Train your mind and body</Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">Access your account</Heading>
+        
+        <Input placeholder='E-mail' />
+        <Input placeholder='Password' />
+      </Center>
     </VStack>
   )
 }
